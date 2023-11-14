1c805b669e17e74e90adaf7da87ad397119583ac
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-11-14 17:44:59 -0500

    Initial commit

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,26 @@
 type CSSMatch = string;
 type ImportString = string;
+type MediaQuery = string;
+
+export interface ImportContext {
+    mountInit: MountInit,
+    refs:  readonly WeakRef<Element>[],
+
+} 
+export type PipelineProcessor<ReturnType = void> = (element: Element, ctx: ImportContext) => Promise<ReturnType>
 export interface MountInit{
     match: string,
-    within: Element | ShadowRoot | DocumentFragment | Node,
-    import: ImportString | [ImportString, ImportAssertions]
+    within?: Element | ShadowRoot | DocumentFragment | Node,
+    withinStream?: any,
+    intersectionObserverInit?: IntersectionObserverInit,
+    mediaMatches?: MediaQuery,
+    containerQuery?: MediaQuery,
+    ifInstanceOf?: Array<any>
+    actsOn: {
+        instanceOf?: Array<any>, //[TODO] What's the best way to type this?,
+        cssMatch?: string,
+    }
+    import?: ImportString | [ImportString, ImportAssertions] | PipelineProcessor,
+    doCallbackIf?: PipelineProcessor<boolean>,
+    callback?: PipelineProcessor,
 }
